a395d2ca1ad2ad978de6dc6b3800787e93eba0c1
Daniel Morandini <danielmorandini@me.com>
2023-12-05 18:36:13 +0100

    Add test for second part

--- day-2/main.zig
+++ day-2/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
-test "read game configurations" {
+test "possible games" {
     const example: []const u8 =
         \\Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
         \\Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -11,6 +11,17 @@ test "read game configurations" {
     ;
 
     try expect(try findPossible(example, 12, 13, 14) == 8);
+}
+
+test "power of the games" {
+    const example: []const u8 =
+        \\Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        \\Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        \\Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        \\Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        \\Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    ;
+    try expect(try computePower(example) == 2286);
 }
 
 const Game = struct {
